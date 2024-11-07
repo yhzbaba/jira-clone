9c074bcd60bd2a9813895ba3be4eb5a98c80510f
yhzbaba <2679122209@qq.com>
2024-11-07 23:47:05 +0800

    app-write login register logout server sdk

---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "yhz-jira-clone-session"
--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -2,5 +2,4 @@ import {hc} from "hono/client"
 
 import {AppType} from "@/app/api/[[...route]]/route";
 
-console.log(process.env.NEXT_PUBLIC_APP_URL)
 export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!)
